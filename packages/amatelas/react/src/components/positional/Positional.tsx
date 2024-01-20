6e28bc770d3eb18b44cfd61896ccf8e7a565fe63
okmtyuta <okmtyuta@gmail.com>
2024-01-20 18:06:59 +0900

    positional

--- packages/amatelas/react/src/components/positional/Positional.tsx
+++ packages/amatelas/react/src/components/positional/Positional.tsx
@@ -2,11 +2,9 @@ import { PositionalPosition, positionalClasses } from '@okmtyuta/amatelas-theme'
 import clsx from 'clsx'
 import { ElementType, ComponentPropsWithoutRef } from 'react'
 
-type Position = PositionalPosition
-
 export type PositionalProps<T extends ElementType> = {
   as?: T
-  position?: Position
+  position?: PositionalPosition
 } & Omit<ComponentPropsWithoutRef<T>, 'tag'>
 
 const classes = positionalClasses
@@ -14,19 +12,23 @@ const classes = positionalClasses
 export const Positional = <T extends ElementType = 'div'>({
   as,
   position,
-  className,
   ...props
 }: PositionalProps<T>) => {
   const _Element = as ?? 'div'
 
   if (!position || position === 'static') {
-    return <_Element {...props} className={clsx(classes.positional, className)} />
+    return (
+      <_Element
+        {...props}
+        className={clsx(classes.positional, props.className)}
+      />
+    )
   }
 
   return (
     <_Element
       {...props}
-      className={clsx(classes.positional, classes[position], className)}
+      className={clsx(classes.positional, classes[position], props.className)}
     />
   )
 }
